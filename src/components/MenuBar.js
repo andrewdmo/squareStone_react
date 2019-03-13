4841b074880c39303c0c9790d4ccdb3dfdaa74e4
andrewdmo <andrewdmo@gmail.com>
2019-03-13 14:37:50 -0400

    -top work'

--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -4,17 +4,21 @@ import React, {Component} from "react";
 
 export default class MenuBar extends Component {
 
+    constructor(props) {
+        super(props);
+        this.state = {page: "menuText shadow CURRENT"};
+    }
+
     render() {
         return (
-            <div className="ten columns menuBar">
-                <h1 className="topTitle shadow">AG3: the Future of Now</h1>
-
-                <p className="menuText shadow current">Home</p>
-
-                <p className="menuText shadow none">Mapping
+            <div className="menuBar">
+                <p className={this.state.page}>Home
                 </p>
 
-                <p className="menuText shadow none">About
+                <p className="menuText shadow NONE">Mapping
+                </p>
+
+                <p className="menuText shadow NONE">About
                 </p>
             </div>
         )
